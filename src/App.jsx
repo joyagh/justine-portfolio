e3a4c20cda2872b113940f8dc903b31b2a5a393d
Justine Oyaghiro <justineoyaghiro@icloud.com>
2023-09-21 18:26:56 -0500

    Create About component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./components/Header"
 import "bootstrap/dist/css/bootstrap.min.css";
+import About from "./components/About";
 
 class App extends React.Component {
   constructor() {
@@ -11,10 +12,14 @@ class App extends React.Component {
  
   render() {
     return (
+      <>
     <Header />
+    <About />
+      </>
     
     )
   }
+}
 }
 
 export default App;
